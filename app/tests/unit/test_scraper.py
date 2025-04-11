cb406d70f734a368912e873af92b263b18de7e3a
Do Dinh Trung <161141020+trung2604@users.noreply.github.com>
2025-04-11 22:57:31 +0700

    Add project structure creation script and update test URLs

--- app/tests/unit/test_scraper.py
+++ app/tests/unit/test_scraper.py
@@ -2,7 +2,7 @@ from app.utils.Scraper.scraper import scrape
 
 def main():
     # URL to test the scrape function
-    url = "https://www.dailymail.co.uk/tvshowbiz/article-14588373/White-Lotus-friendships-Aimee-Lou-Wood-Walton-Goggins-feud.html"
+    url = "https://e.vnexpress.net/news/tech/enterprises/tech-giant-fpt-partners-with-chelsea-fc-for-global-digital-transformation-4872335.html?fbclid=IwY2xjawJleDdleHRuA2FlbQIxMAABHkA70XlV78dbh5HziH6E9kqDl7liblVqbf7_NGna_lkB7TyM_S10kFGkx5Hn_aem_qFDxmN08Y8CR61GEJsgCIg"
 
     # Call the scrape function
     result = scrape(url)
